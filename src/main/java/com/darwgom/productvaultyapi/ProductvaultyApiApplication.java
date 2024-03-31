0ad7c535e700e3f9d63d1611d0dcea49e8488751
darwgom7 <darwgom7@gmail.com>
2024-03-31 17:19:28 -0500

    Initial commit

---
+++ src/main/java/com/darwgom/productvaultyapi/ProductvaultyApiApplication.java
@@ -0,0 +1,13 @@
+package com.darwgom.productvaultyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductvaultyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductvaultyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darwgom/productvaultyapi/ProductvaultyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwgom.productvaultyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductvaultyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
